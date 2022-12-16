e6d7b83adeae640d48a04fc6df54dd88d6478f2a
DeekshaVarshney12 <103417247+DeekshaVarshney12@users.noreply.github.com>
2022-12-16 13:38:56 +0000

    Update provider.tf

--- demoaks/provider.tf
+++ demoaks/provider.tf
@@ -10,6 +10,6 @@ terraform {
     storage_account_name = "strogaer343"
     container_name       = "newcontainer"
     key                  = "prod.terraform.tfstate"
-    access_key = "ulKMQ35d7YipEVIwONoOYRv3d4DyG8kHuf1E5tMa4YOqnq4W1I7sgWJ623DZT22ClnEaTqd3e4Yu+AStNlQDzw=="
+    access_key = "k88bgwpiWQkeb5S9e4VpRQIL4cQSNj3OwlobHTBy7bOJlIxeY1J0cTq9HzJPD2lvpp/YP5Lh9Pe5+AStDsF7aA=="
   }
 }
